947c04606d151af6cf7c559b7220400497a9bb07
Carlo Zottmann (Rupert) <carlo@zottmann.org>
2010-08-31 00:33:09 +0200

    Added search mask w/ autocomplete.

--- js/main.js
+++ js/main.js
@@ -1,4 +1,55 @@
+/*jslint adsafe: false, bitwise: true, browser: true, cap: false, css: false,
+  debug: false, devel: true, eqeqeq: true, es5: false, evil: false,
+  forin: false, fragment: false, immed: true, laxbreak: false, newcap: true,
+  nomen: false, on: false, onevar: true, passfail: false, plusplus: true,
+  regexp: false, rhino: true, safe: false, strict: false, sub: false,
+  undef: true, white: false, widget: false, windows: false */
+/*global jQuery: false, $: false, window: false */
+"use strict";
+
+
 $( document ).ready( function() {
+
+  function initAC() {
+    $( "#search" ).autocomplete( "destroy" );
+
+    $.ajax({
+      url: "index.json",
+      type: "json",
+      complete: function( data ) {
+        var acdata = $.parseJSON( data.responseText ),
+          supply = [];
+
+        $.each( acdata, function( i, dataset ) {
+          var key = "show_" + dataset.access,
+            label = [ dataset.host, " → ",
+              dataset.name,
+              " (", dataset.type, ")"
+            ].join( "" );
+        
+          if ( $.jStorage.get( key, false ) || dataset.access === "" ) {
+            supply.push({
+              label: label,
+              url: dataset.url
+            });
+          }
+        });
+
+        $( "#search" ).autocomplete({
+          source: supply,
+          focus: function( event, ui ) {
+            $('#search').val( ui.item.label );
+            return false;
+          },
+          select: function( event, ui ) {
+            document.location.href = ui.item.url;
+            return false;
+          }
+        });
+      }
+    });
+  }
+
 
   $.each( [ "private", "protected", "deprecated" ], function( i, kind ) {
     var key = "show_" + kind;
@@ -26,6 +77,11 @@ $( document ).ready( function() {
     else {
       $( "." + what ).hide();
     }
+    
+    initAC();
   });
-  
+
+    
+  initAC();
+
 });
